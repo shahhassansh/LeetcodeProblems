840336853931036a120031e25d0d5795d92f3b47
shahhassansh <shahhassan@live.com>
2020-08-21 10:15:40 -0400

    Adding Problem 21

---
+++ mergeTwoLists.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode()
+        out = head
+        p1, p2 = l1, l2
+        while p1 != None and p2 != None:
+            if p1.val <= p2.val:
+                out.next = p1
+                p1 = p1.next
+            else:
+                out.next = p2
+                p2 = p2.next
+            out = out.next
+        if p1 != None:
+            out.next = p1
+            p1 = p1.next
+        if p2 != None:
+            out.next = p2
+            p2=p2.next
+        return head.next
