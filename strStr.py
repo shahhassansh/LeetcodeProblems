43eb0876f08dd4cc039f425b521d2f5e8f7f692d
shahhassansh <shahhassan@live.com>
2020-08-24 18:18:46 -0400

    Updating Problem 28

--- strStr.py
+++ strStr.py
@@ -3,7 +3,6 @@ class Solution:
         if needle == "":
             return 0
         for i in range(len(haystack)):
-            if needle[0] == haystack[i]:
-                if needle == haystack[i:i+len(needle)]:
-                    return i
+            if needle == haystack[i:i+len(needle)]:
+                return i
         return -1
