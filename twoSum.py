d3fa5b6c39415d6185adc9434d1c01a92a477751
shahhassansh <shahhassan@live.com>
2024-02-25 14:05:26 -0800

    Updating Probelm 1

--- twoSum.py
+++ twoSum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        my_dict = dict()
-        arr = []
+        d = {}
         for i in range(0,len(nums)):
-            k = target - nums[i]
-            if k in my_dict:
-                arr.append(my_dict[k])
-                arr.append(i)
-                return arr
+            if target - nums[i] not in d:
+                d[nums[i]] = i
             else:
-                my_dict[nums[i]] = i
+                return [d[target - nums[i]],i]
