8e19491e1ab4bc984c434b26a308aa782f9a8071
shahhassansh <shahhassan@live.com>
2025-04-10 20:20:05 -0700

    Adding Problem 1445

---
+++ sql/Apples_and_Oranges.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select sale_date, 
+sum(case when fruit = 'apples' then sold_num else 0 end) - sum(case when fruit = 'oranges' then sold_num else 0 end) as diff
+from Sales
+group by 1
+order by 1;
