b7c0ddd88d003f404f63b77c91ebf907607b9d3f
shahhassansh <shahhassan@live.com>
2025-04-12 22:46:01 -0700

    Adding Problem 1747

---
+++ sql/Leetflex_Banned_Accounts.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select distinct a.account_id
+from LogInfo a
+join 
+LogInfo b 
+on a.account_id = b.account_id
+and a.ip_address != b.ip_address
+and (a.login between b.login and b.logout
+or b.login between a.login and a.logout);
