2b806e4c9eebbe1e744e2cdecb91c94ec8724279
shahhassansh <shahhassan@live.com>
2025-12-22 22:33:58 -0800

    Adding Problem 155

---
+++ python3/MinStack.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+        self.st = []        
+
+    def push(self, val: int) -> None:
+        mn = self.getMin()
+        if mn == None or val < mn:
+            mn = val
+        self.st.append([val,mn])
+        
+
+    def pop(self) -> None:
+        self.st.pop()
+        
+
+    def top(self) -> int:
+        if self.st:
+            return self.st[-1][0]
+        
+
+    def getMin(self) -> int:
+        if self.st:
+            return self.st[-1][1]
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
