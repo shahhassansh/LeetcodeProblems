a31ed9db8802be86e80cb8b1b0b898c117ff16d3
shahhassansh <shahhassan@live.com>
2025-12-22 21:54:31 -0800

    Adding Problem 735

--- python3/asteroidCollision.py
+++ python3/asteroidCollision.py
@@ -4,21 +4,17 @@ class Solution:
         for a in input:
             if len(st) == 0:
                 st.append(a)
-            else:
-                if a > 0:
-                    st.append(a)
-                elif a <0 and st[-1] <0:
-                    st.append(a)
-                else:
-                    if st[-1] > 0:
-                        if st[-1] < -1*a:
-                            st[-1] = a
-                        elif st[-1] == -1*a:
-                            st.pop()
+            elif a > 0 or (a <0 and st[-1] <0):
+                st.append(a)
+            elif st[-1] > 0:
+                if st[-1] < -1*a:
+                    st[-1] = a
+                elif st[-1] == -1*a:
+                    st.pop()
         return st
 
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        st = []
         while asteroids != self.itr_collision(asteroids):
             asteroids = self.itr_collision(asteroids)
         return asteroids
+        
