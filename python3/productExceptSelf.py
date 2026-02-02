15114dcd67299f9bf2777824aa03b75453cd97be
shahhassansh <shahhassan@live.com>
2026-02-01 17:47:48 -0800

    Adding Problem 238

--- python3/productExceptSelf.py
+++ python3/productExceptSelf.py
@@ -1,15 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1]*len(nums)
-        right = [1]*len(nums)
-        result = [1]*len(nums)
-        left[0] = 1
-        right[len(nums)-1] = 1
+        output = [1]* len(nums)
+        left = 1
+        right = 1
 
-        for i in range(1,len(nums)):
-            left[i] = nums[i-1]*left[i-1]
-            result[i] = left[i]*right[i]
-        for i in range(len(nums)-2,-1,-1):
-            right[i] = nums[i+1]*right[i+1]
-            result[i] = left[i]*right[i]
-        return result
+        for i in range(len(nums)):
+            output[i] *= left
+            left *= nums[i]
+        
+        for i in range(len(nums)-1,-1,-1):
+            output[i] *= right 
+            right *= nums[i]
+
+        return output 
+        
