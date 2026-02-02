342794a7acb0f0da41f6ddae8c6b40e4f202e167
shahhassansh <shahhassan@live.com>
2026-02-01 18:44:40 -0800

    Adding Problem 283

---
+++ python3/moveZeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        idx = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[idx], nums[i] = nums[i], nums[idx]
+                idx+=1
+        return nums
+
+
+
+        
