836f3155aa86b21e5389495547133a7ee0e497ef
shahhassansh <shahhassan@live.com>
2026-02-03 20:51:59 -0800

    Adding Problem 1004

--- python3/longestOnes.py
+++ python3/longestOnes.py
@@ -1,14 +1,15 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        i = 0
-        j = 0
-        while j <len(nums):
-            if nums[j] == 0:
-                k-=1
-            j+=1
-            if k<0:
-                if nums[i] == 0:
-                    k+=1
-                i+=1
-        return j-i
-       
+        left = 0
+        max_ones = 0
+        zero_count = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count +=1
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -=1
+                left+=1
+            max_ones = max(max_ones, right - left +1)
+        return max_ones
+        
